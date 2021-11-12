d701ae7e35f482d9fb718d986c2dca022c6b92d6
Yevhenii <freshjs.fte@gmail.com>
2021-11-12 19:33:49 +0200

    Add user validation

--- migrations/20211111182521-create-user.js
+++ migrations/20211111182521-create-user.js
@@ -11,11 +11,15 @@ module.exports = {
         primaryKey: true
       },
 
+
+
       firstName: {
+        field: 'first_name',
         allowNull: false,
         type: Sequelize.STRING
       },
       lastName: {
+        field: 'last_name',
         allowNull: false,
         type: Sequelize.STRING
       },
@@ -23,16 +27,27 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
+      birthdate: {
+        type: Sequelize.DATE,
+      },
+      isSubscribed: {
+        field: 'is_subscribed',
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+
 
       createdAt: {
+        field: 'created_at',
         allowNull: false,
         type: Sequelize.DATE
       },
       updatedAt: {
+        field: 'updated_at',
         allowNull: false,
         type: Sequelize.DATE
       }
-
 
     });
   },
--- models/user.js
+++ models/user.js
@@ -1,30 +1,75 @@
 "use strict";
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    static associate(models) {}
+    static associate(models) {
+
+    }
   }
+
+  /* 
+  Создайте сущность
+  Task (body, isDone, deadline)
+  Добавьте ограничения в миграцию и в модель
+  Добавьте валидацию в модель
+*/
 
   User.init(
     {
       firstName: {
+        field: 'first_name',
         type: DataTypes.STRING,
-        allowNull: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        }
       },
       lastName: {
+        field: 'last_name',
         type: DataTypes.STRING,
-        allowNull: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        }
       },
       email: {
         type: DataTypes.STRING,
-        allowNull: true,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        }
       },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        }
+      },
+      birthdate: {
+        type: DataTypes.DATE,
+        validate: {
+          isDate: true,
+        }
+      },
+      isSubscribed: {
+        field: 'is_subscribed',
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      }
     },
 
     {
       sequelize,
       modelName: "User",
       tableName: "users",
+      // underscored: true,
     }
   );
   return User;
