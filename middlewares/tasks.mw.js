8eac10164debd9a35aba63db354f98064e8b9c88
Yevhenii <freshjs.fte@gmail.com>
2021-11-15 19:50:21 +0200

    Add return statements

--- middlewares/tasks.mw.js
+++ middlewares/tasks.mw.js
@@ -1,7 +1,7 @@
 module.exports.validateTask = (req, res, next) => {
   const data = req.body;
   if (data.body === "") {
-    next(new TypeError("Body should not be empty string"));
+    return next(new TypeError("Body should not be empty string"));
   }
-  next();
+  return next();
 };
